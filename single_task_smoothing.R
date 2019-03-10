5d3929303e733ac5a61ee9377a796c7312171a61
Jie Peng <jiepeng@Peng-MBP-2.local>
2019-03-09 23:19:00 -0800

    typo correct

--- single_task_smoothing.R
+++ single_task_smoothing.R
@@ -23,7 +23,7 @@ v.res <- v.smooth.bw(pre=pre, braingrid=braingrid, len=50,
                       range1=c(0.005,0.3), range2=c(0.005,0.3), cpus=ncpu,
                       xy.truncrange=100, z.truncrange=100, thres.v0.w=0.2, K=5,
                       cv.method="mad") ##mcv
-v.obj <- update.v.obj(pre=pre, res=v.res3)
+v.obj <- update.v.obj(pre=pre, res=v.res)
 
 ##save results
 save(v.obj, file="smoothing.Rdata") ##this will be used as inputs for fiber tracking 
